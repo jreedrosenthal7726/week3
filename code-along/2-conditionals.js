2ae6f17563ee08fbb71e9f08c6e21a7e717078d6
Josh Rosenthal <josh.rosenthal@kellogg.northwestern.edu>
2021-04-15 20:57:33 -0500

    Final

--- code-along/2-conditionals.js
+++ code-along/2-conditionals.js
@@ -16,5 +16,36 @@ function getRandomInt(max) {
 
 // input is a variable which contains the user's input
 let input = prompt(`Enter a number between 1 and 10`)
+console.log(input)
 
 // Write the recipe (algorithm) in the comments. Then, write the code.
+
+// Store a random integer between 1 and 10 in memory;
+let randomNumber = getRandomInt(10)
+//produce a 
+//   human-readable output to the JavaScript Console in Chrome showing 
+//   the number. 
+console.log(`the randon number is ${randomNumber}`)
+
+// - Then, show the number the user entered ("input"). 
+console.log(`you entered ${input}`)
+
+// - If the user did not enter a number between 1-10, display an error
+//   message.  
+
+// if (3 < 3) {
+//   console.log(`it was true`)
+// } else {
+//   console.log(`it was false`)
+// }
+// - If the user guessed the number, display "YOU WIN!"
+// - If the user did not guess the number, display "SORRY!"
+let message
+if (input < 1 || input >10) {
+  message.log(`Did you read the instructions?!`)
+} else if (input == randomNumber) {
+  message.log(`YOU WIN!`)
+} else {
+  message.log(`SORRY!`)
+}
+console.log(message)
--- practice/2-conditionals.js
+++ practice/2-conditionals.js
@@ -8,6 +8,29 @@ function getRandomInt(max) {
 // - Begin with the recipe (algorithm) and code from the first practice
 // - Implement the (basic) rules of craps:
 //   - If the total of the two dice is 7 or 11, the player wins
+let die1 = getRandomInt(6)
+let die2 = getRandomInt(6)
+let total = die1 + die2
+console.log(`Die 1 was ${die1}`)
+console.log(`Die 2 was ${die2}`)
+console.log(`Total was ${total}`)
+
+if (total == 7 || total == 11) {
+  console.log(`You win!`)
+} else if (total == 2 || total == 3 || total == 12) {
+  console.log(`You lose!`)
+} else {
+  console.log(`the point is ${total}`)
+}
+
+if (total == 7 || total == 11) {
+  console.log(`YOU WIN!`)
+} else if (total == 2 || total == 3 || total == 12) {
+  console.log(`YOU LOSE!`)
+} else {
+  console.log(`The point is ${total}`)
+}
+
 //   - If the total of the two dice is 2, 3, or 12, the player loses
 //   - If the total is anything else, the "point" is set, e.g. "the point is 6"
 
